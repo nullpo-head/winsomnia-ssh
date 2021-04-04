782d623413e2034739b88cad915357f014001ac4
Takaya Saeki <abc.tkys+pub@gmail.com>
2021-04-04 23:35:16 +0900

    Fix a bug and make winsomnia run in the background

--- winsomnia_ssh/winsomnia_ssh.py
+++ winsomnia_ssh/winsomnia_ssh.py
@@ -4,6 +4,7 @@ import argparse
 import subprocess
 import psutil
 import sys
+import os
 import logging
 import time
 from typing import Optional
@@ -26,17 +27,20 @@ def main():
         logging.critical(err)
         sys.exit(1)
 
-    if not is_ssh_session():
+    session = CurrentSshSession()
+    if not session.is_ssh_session():
         logging.info("exiting because this is not a ssh session")
         sys.exit(0)
-    logging.info("Detected winsomnia-ssh is running in a ssh session.")
 
-    logging.info("Keep Windows awake")
-    parent = psutil.Process().parent()
-    while parent.is_running():
-        if not prevent_winsleep(3):
-            logging.critical("winsomnia failed")
-            sys.exit(1)
+    # daemonize here because the shell that launched this process in its "~/.*rc" is waiting
+    daemonize()
+
+    logging.info("Detected winsomnia-ssh is running in a ssh session.")
+    while session.is_alive():
+        wait_completed_process_noblock()  # let zombie processes exit
+        # Allow two processes to run at the same time for a minute
+        # so that there is no time when there is not a single winsomnia process.
+        run_winsomnia_noblock(3)
         time.sleep(120)
 
 
@@ -53,13 +57,42 @@ def check_prerequisite() -> Optional[str]:
     return None
 
 
-def is_ssh_session() -> bool:
-    return any(filter(lambda p: p.name() == "sshd", psutil.Process().parents()))
+def daemonize():
+    if os.fork() == 0:
+        return
+    else:
+        sys.exit(0)
 
 
-def prevent_winsleep(timeout: int) -> bool:
-    result = subprocess.run(["winsomnia", str(timeout), "-q"])
-    return result.returncode == 0
+def run_winsomnia_noblock(timeout: int):
+    subprocess.Popen(["winsomnia", str(timeout), "-q"],
+                     stdin=subprocess.DEVNULL)
+
+
+def wait_completed_process_noblock():
+    while True:
+        try:
+            if os.waitpid(0, os.WNOHANG) == (0, 0):
+                break
+        except ChildProcessError:
+            # ignore no-child error
+            break
+
+
+class CurrentSshSession:
+    def __init__(self):
+        self.parents = psutil.Process().parents()
+        self._is_ssh_session = None
+
+    def is_ssh_session(self) -> bool:
+        if self._is_ssh_session:
+            return self._is_ssh_session
+        self._is_ssh_session = any(
+            filter(lambda p: p.name() == "sshd", self.parents))
+        return self._is_ssh_session
+
+    def is_alive(self) -> False:
+        return self.is_ssh_session() and self.parents[0].is_running()
 
 
 if __name__ == "__main__":
